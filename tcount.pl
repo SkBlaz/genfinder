0121fd8f5baf4bb3aa4e98ee4945cf4bf17805b0
SkBlaz <skrljblaz@gmail.com>
2016-10-14 18:59:04 +0200

    Improved criteria

--- tcount.pl
+++ tcount.pl
@@ -39,72 +39,79 @@ while (my $line = <$in>) {
     
     my @tempsplit = split /\t/, $line;
 
-    if (length($tempsplit[9]) > $bdrs[0]){
-	my $sequence = substr($tempsplit[9], $bdrs[0], ($bdrs[1]-$bdrs[0]));	
-	print ($sequence."\n");
-	my $sout = ">".$ident."\n".$sequence."\n";
-	print $fh2 $sout;
-    }
+    # if (length($tempsplit[9]) >= $bdrs[1]){
+    # 	my $sequence = substr($tempsplit[9], $bdrs[0], ($bdrs[1]-$bdrs[0]));	
+    # 	print ($sequence." ".length($tempsplit[9])."\n");
+    # 	my $sout = ">".$ident."\n".$sequence."\n";
+    # 	print $fh2 $sout;
+    # }
 
+    ## ce je ident enak imenu npr VVS2
     if ($ident == $tempsplit[2]){
-	
-	if ($tempsplit[5] !~ /^S/){
+	if (length($tempsplit[9]) >= $bdrs[1]){	
+	    if ($tempsplit[5] !~ /^S/){
 
 
-	    (my @Ms) = $tempsplit[5] =~ /\d{1,3}M/g;
-	    (my @Is) = $tempsplit[5] =~ /\d{1,3}[I]/g;
-	    (my @Ds) = $tempsplit[5] =~ /\d{1,3}[D]/g;
-	    (my @all) = $tempsplit[5] =~ /\d{1,3}/g;
-	    
-### za prvi entry pogleda, kje je glede na region. ce je pod, potem bo prvemu pristet drugi, dokler ni nad prvo region. Pogledamo koliko nad region je, pristejemo to k dolzini	    
-	    
-	    my $Msum = 0;
-	    my $baseLen = 0;
-	    my $insertion = 0;
-	    my $deletion = 0;
-	    foreach $entry (@all){
-		foreach $match (@Ms){
-		    if ($entry == $match){
-			print($entry." ".$match."\n");
-			$Msum += $match;
-			#print($Msum."\n");
-			if ($bdrs[0] < $Msum && $bdrs[1] > $Msum){
-			    print ("lower bound: ".$bdrs[0]." ".$Msum."\n");
-			    $baseLen += ($Msum - $bdrs[0]);
-			}elsif ($Msum < $bdrs[1] && $Msum > $bdrs[0]){
-			    print ("MIddle bound: ".$bdrs[1]." ".$Msum."\n");
-			    $baseLen += ($match-$bdrs[0]);
-			}elsif ($Msum > $bdrs[1]){
-			    print ("Higher bound..\n");
-			    $baseLen = ($bdrs[1] - ($bdrs[0]));
-			}		   
-		    }
-		    
-		}	   
-	    }
-	    if ($Msum >= $bdrs[1]){
-		print ("....BEG....\n");
-		#print ($tempsplit[5]."\n");
-		for $sig (@Is){
-		    $sig =~ s/[A-Z]+//;
-		    print ($sig."iiiiii\n");
-		    $baseLen += $sig;
-		}		# 
-		for $dig (@Ds){
-		    $dig =~ s/[A-Z]+//;
-		    print ($dig."ddddd\n");
-		    $baseLen -= $dig;
+		(my @Ms) = $tempsplit[5] =~ /\d{1,3}M/g;
+		(my @Is) = $tempsplit[5] =~ /\d{1,3}[I]/g;
+		(my @Ds) = $tempsplit[5] =~ /\d{1,3}[D]/g;
+		(my @all) = $tempsplit[5] =~ /\d{1,3}/g;
+		
+		### za prvi entry pogleda, kje je glede na region. ce je pod, potem bo prvemu pristet drugi, dokler ni nad prvo region. Pogledamo koliko nad region je, pristejemo to k dolzini	    
+		
+		my $Msum = 0;
+		my $baseLen = 0;
+		my $insertion = 0;
+		my $deletion = 0;
+		foreach $entry (@all){
+		    foreach $match (@Ms){
+			if ($entry == $match){
+			    print($entry." ".$match."\n");
+			    $Msum += $match;
+			    #print($Msum."\n");
+			    if ($bdrs[0] < $Msum && $bdrs[1] > $Msum){
+				print ("lower bound: ".$bdrs[0]." ".$Msum."\n");
+				$baseLen += ($Msum - $bdrs[0]);
+			    }elsif ($Msum < $bdrs[1] && $Msum > $bdrs[0]){
+				print ("MIddle bound: ".$bdrs[1]." ".$Msum."\n");
+				$baseLen += ($match-$bdrs[0]);
+			    }elsif ($Msum > $bdrs[1]){
+				print ("Higher bound..\n");
+				$baseLen = ($bdrs[1] - ($bdrs[0]));
+			    }		   
+			}
+			
+		    }	   
 		}
+		if ($Msum >= $bdrs[1]){
+		    print ("....BEG....\n");
+		    #print ($tempsplit[5]."\n");
+		    for $sig (@Is){
+			$sig =~ s/[A-Z]+//;
+			print ($sig."iiiiii\n");
+			$baseLen += $sig;
+		    }		# 
+		    for $dig (@Ds){
+			$dig =~ s/[A-Z]+//;
+			print ($dig."ddddd\n");
+			$baseLen -= $dig;
+		    }
 
-		push(@frequencies, $baseLen.","); 
-		print ("\n base length: ".$baseLen."\n");
-		print ("Match sum: ".$Msum."\n");	
-		print ("....END....\n");    
-	    }	    
+		    my $sequence = substr($tempsplit[9], $bdrs[0], ($bdrs[1]-$bdrs[0]));	
+		    print ($sequence." ".length($tempsplit[9])."\n");
+		    my $sout = ">".$ident."\n".$sequence."\n";
+		    print $fh2 $sout;
+		    
+		    push(@frequencies, $baseLen.","); 
+		    print ("\n base length: ".$baseLen."\n");
+		    print ("Match sum: ".$Msum."\n");	
+		    print ("....END....\n");    
+		}	    
+	    }
+	    
 	}
     }
 }
-
 close $in;
 
 #close $fhR;
@@ -115,9 +122,9 @@ close $in;
 #     print $x.",";  
 #     my $count = grep ($x, @frequencies);
 #     print $count.",";
- 
+
 # }
-    
+
 # my $count = ($string =~ s/a/a/g);
 
 # foreach my $it (@frequencies){
@@ -137,7 +144,9 @@ while (my ($key, $value) = each(%count)) {
 
 #output the counts
 while (my ($key, $value) = each(%count)) {
-    print $fh "$key:$value\n";
+    if ($key > 0){
+	print $fh  "$key$value\n"; #$fh
+    }
 }
 close $fh;
 close $fh2;
